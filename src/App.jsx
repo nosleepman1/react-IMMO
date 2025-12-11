02477afbeeb77572ec1b9b6a97e95e646ab74e23
nosleepman1 <abbdallahdiouf.dev@gmail.com>
2025-12-11 02:39:38 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
+
+
+
+function App() {
+
+
+  return (
+    <Routes>
+
+      <Route path="/" element  />
+
+    </Routes>
+  )
+}
+
+export default App
